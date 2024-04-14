47ed4559400b83e42a5ab8318262fd3fc3046b00
Matheus Oliveira <matheusoliveira0890@gmail.com>
2024-04-14 00:34:03 -0300

    primeiro commit

---
+++ src/main/java/com/matheusob25/projetoweb/ProjetoWebApplication.java
@@ -0,0 +1,14 @@
+package com.matheusob25.projetoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoWebApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ProjetoWebApplication.class, args);
+	}
+
+}
