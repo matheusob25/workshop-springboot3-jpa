4e9cd193fc235e2774f8ccceb0bfd02533c1fa51
Matheus Oliveira <matheusoliveira0890@gmail.com>
2024-04-16 00:49:42 -0300

    service layer, component registration

--- src/main/java/com/matheusob25/projetoweb/repositories/UserRepository.java
+++ src/main/java/com/matheusob25/projetoweb/repositories/UserRepository.java
@@ -3,6 +3,7 @@ package com.matheusob25.projetoweb.repositories;
 import com.matheusob25.projetoweb.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
