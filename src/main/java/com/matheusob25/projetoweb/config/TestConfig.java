bcc06945a6f7039fefc16a97918e96adcc2cc244
Matheus Oliveira <matheusoliveira0890@gmail.com>
2024-04-17 19:10:28 -0300

    Jointable de relacionamento muitos para muitos entre product e category

--- src/main/java/com/matheusob25/projetoweb/config/TestConfig.java
+++ src/main/java/com/matheusob25/projetoweb/config/TestConfig.java
@@ -41,16 +41,23 @@ public class TestConfig implements CommandLineRunner {
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITING_PAYMENT ,u2);
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.PAID ,u1);
 
-        Category c1 = new Category(null, "fit");
-        Category c2 = new Category(null, "utility");
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
 
-        categoryRepository.saveAll(Arrays.asList(c1, c2));
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
         Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
         Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
         Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
         Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
         Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 
+        p1.getCategories().add(cat2);
+        p2.getCategories().add(cat3);
+        p2.getCategories().add(cat1);
+        p3.getCategories().add(cat1);
+        p4.getCategories().add(cat1);
+        p5.getCategories().add(cat2);
 
         productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
         userRepository.saveAll(Arrays.asList(u1, u2));
