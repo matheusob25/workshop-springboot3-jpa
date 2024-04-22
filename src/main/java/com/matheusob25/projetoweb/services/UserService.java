f27a5a341db0be1251b08cb490d511066b2934f8
Matheus Oliveira <matheusoliveira0890@gmail.com>
2024-04-22 01:54:04 -0300

    user delete

--- src/main/java/com/matheusob25/projetoweb/services/UserService.java
+++ src/main/java/com/matheusob25/projetoweb/services/UserService.java
@@ -25,4 +25,7 @@ public class UserService {
     public User insert(User user){
        return userRepository.save(user);
     }
+    public void delete(Long id){
+        userRepository.deleteById(id);
+    }
 }
