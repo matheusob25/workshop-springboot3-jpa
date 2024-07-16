6ef0f22cf085fdb36869ccf73565aa6dbba17157
Matheus Oliveira <matheusoliveira0890@gmail.com>
2024-07-15 23:58:43 -0300

    organizando códigos do front end

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,42 +1,62 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const cadastroForm = document.querySelector("#cadastro-form");
-    const loginForm = document.querySelector("#login-form");
+document.getElementById("cadastro-form").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const xname = document.querySelector(".name");
+    const xemail = document.querySelector(".email");
+    const xphone = document.querySelector(".phone");
+    const xpassword = document.querySelector(".password");
+
+    if (!xemail || !xpassword || !xname || !xphone || !xpassword) {
+        alert('Por favor, preencha todos os campos.');
+        return;
+    }
+
+    if (xpassword.length < 6) {
+        alert('A senha deve ter pelo menos 6 caracteres.');
+        return;
+    }
+
+    // Se todas as validações passarem, enviar os dados
+    cadastrar(xname, xemail, xphone, xpassword);
+    document.getElementById("cadastro-form").reset();
+});
+
+function cadastrar(name, email, phone, password) {
    
 
-
-    // Função para cadastrar
-    function cadastrar() {
-        const xname = cadastroForm.querySelector(".name");
-        const xemail = cadastroForm.querySelector(".email");
-        const xphone = cadastroForm.querySelector(".phone");
-        const xpassword = cadastroForm.querySelector(".password");
-
-        fetch("http://localhost:8080/users", {
-            headers: {
-                "Accept": "application/json",
-                "Content-Type": "application/json"
-            },
-            method: "POST",
-            body: JSON.stringify({
-                name: xname.value,
-                email: xemail.value,
-                phone: xphone.value,
-                password: xpassword.value
-            })
+    fetch("http://localhost:8080/users", {
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        method: "POST",
+        body: JSON.stringify({
+            name: name.value,
+            email: email.value,
+            phone: phone.value,
+            password: password.value
         })
-            .then(function (response) {
-                console.log(response);
-                if (response.ok) {
-                    alert('Cadastro realizado com sucesso!');
-                } else {
-                    alert('Erro no cadastro');
-                }
-            })
-            .catch(function (error) {
-                console.error('Error:', error);
+    })
+        .then(function (response) {
+            console.log(response);
+            if (response.ok) {
+                alert('Cadastro realizado com sucesso!');
+            } else {
                 alert('Erro no cadastro');
-            });
-    }
+            }
+        })
+        .catch(function (error) {
+            console.error('Error:', error);
+            alert('Erro no cadastro');
+        });
+}
+
+
+
+document.addEventListener("DOMContentLoaded", function () { 
+    const loginForm = document.querySelector("#login-form");
+   
+   
 
     // Função para login
     function login() {
@@ -72,14 +92,6 @@ document.addEventListener("DOMContentLoaded", function () {
             });
     }
 
-    
-
-    // Event listener para o formulário de cadastro
-    cadastroForm.addEventListener("submit", function (event) {
-        event.preventDefault();
-        cadastrar();
-        cadastroForm.reset(); // Limpar o formulário após o envio
-    });
 
     // Event listener para o formulário de login
     loginForm.addEventListener("submit", function (event) {
